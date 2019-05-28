5054ac1cdf0ee1b7c716cea2ed65868ecf506624
Joe Jevnik <joe@quantopian.com>
2019-05-27 23:55:08 -0400

    ENH: add support for homogeneous boxed types

--- jlist/jlist.h
+++ jlist/jlist.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
+#include <cstring>
 #include <optional>
 #include <vector>
 
@@ -8,34 +10,16 @@
 
 namespace jl {
 enum class entry_tag : std::int8_t {
-    as_ob = 0,
-    as_int = 1,
-    as_double = 2,
-    unset = 3,
+    as_homogeneous_ob = 0,
+    as_heterogeneous_ob = 1,
+    as_int = 2,
+    as_double = 3,
+    unset = 4,
 };
 
-namespace detail {
-template<typename T>
-struct entry_tag_for_type;
-
-template<>
-struct entry_tag_for_type<PyObject*> {
-    static constexpr entry_tag value = entry_tag::as_ob;
-};
-
-template<>
-struct entry_tag_for_type<std::int64_t> {
-    static constexpr entry_tag value = entry_tag::as_int;
-};
-
-template<>
-struct entry_tag_for_type<double> {
-    static constexpr entry_tag value = entry_tag::as_double;
-};
-}  // namespace detail
-
-template<typename T>
-constexpr entry_tag entry_tag_for_type = detail::entry_tag_for_type<T>::value;
+inline bool is_object_tag(entry_tag tag) {
+    return tag == entry_tag::as_homogeneous_ob || tag == entry_tag::as_heterogeneous_ob;
+}
 
 union entry {
     PyObject* as_ob;
@@ -44,7 +28,37 @@ union entry {
 };
 
 template<typename T>
+constexpr bool is_entry_type = std::is_same_v<T, PyObject*> ||
+                               std::is_same_v<T, std::int64_t> ||
+                               std::is_same_v<T, double>;
+
+template<typename T>
+void static_assert_is_entry_type() {
+    static_assert(is_entry_type<T>, "T must be PyObject*, std::int64_t, or double");
+}
+
+namespace detail {
+template<typename T>
+struct entry_pytype;
+
+template<>
+struct entry_pytype<std::int64_t> {
+    constexpr static PyTypeObject* value = &PyLong_Type;
+};
+
+template<>
+struct entry_pytype<double> {
+    constexpr static PyTypeObject* value = &PyFloat_Type;
+};
+}  // namespace detail
+
+template<typename T>
+constexpr PyTypeObject* entry_pytype = detail::entry_pytype<T>::value;
+
+template<typename T>
 T& entry_value(entry& e) {
+    static_assert_is_entry_type<T>();
+
     if constexpr(std::is_same_v<T, PyObject*>) {
         return e.as_ob;
     }
@@ -53,14 +67,13 @@ T& entry_value(entry& e) {
     }
     else if constexpr (std::is_same_v<T, double>) {
         return e.as_double;
-    }
-    else {
-        __builtin_unreachable();
     }
 }
 
 template<typename T>
 const T& entry_value(const entry& e) {
+    static_assert_is_entry_type<T>();
+
     if constexpr(std::is_same_v<T, PyObject*>) {
         return e.as_ob;
     }
@@ -69,9 +82,6 @@ const T& entry_value(const entry& e) {
     }
     else if constexpr (std::is_same_v<T, double>) {
         return e.as_double;
-    }
-    else {
-        __builtin_unreachable();
     }
 }
 
@@ -127,23 +137,69 @@ std::optional<double> maybe_unbox(PyObject* ob) {
     return PyFloat_AsDouble(ob);
 }
 
+namespace detail {
+class tagged_type_pointer {
+private:
+    static_assert(alignof(PyTypeObject) == 8,
+                  "PyTypeObject isn't 8 byte aligned, so we can't store the tag in the "
+                  "low order 3 bits");
+
+    constexpr static std::intptr_t tag_mask = 7;
+    std::intptr_t m_value;
+
+public:
+    tagged_type_pointer(entry_tag tag, PyTypeObject* ptr)
+        : m_value(static_cast<std::uint8_t>(tag) & reinterpret_cast<std::intptr_t>(ptr)) {
+    }
+
+    inline entry_tag tag() const {
+        return static_cast<entry_tag>(m_value & tag_mask);
+    }
+
+    inline void tag(entry_tag tag) {
+        m_value &= ~tag_mask;
+        m_value |= static_cast<std::uint8_t>(tag);
+    }
+
+    inline PyTypeObject* ptr() const {
+        return reinterpret_cast<PyTypeObject*>(m_value);
+    }
+
+    inline void ptr(PyTypeObject* ptr) {
+        m_value &= tag_mask;
+        m_value |= reinterpret_cast<std::intptr_t>(ptr);
+    }
+};
+}  // namespace detail
+
 struct jlist {
     PyObject base;
-    entry_tag tag;
+    detail::tagged_type_pointer tagged_ptr;
     std::vector<entry> entries;
+
+    entry_tag tag() const {
+        return tagged_ptr.tag();
+    }
+
+    void tag(entry_tag tag) {
+        tagged_ptr.tag(tag);
+    }
+
+    bool boxed() const {
+        return is_object_tag(tag());
+    }
+
+    PyTypeObject* homogeneous_type_ptr() const {
+        return tagged_ptr.ptr();
+    }
+
+    void homogeneous_type_ptr(PyTypeObject* ptr) {
+        tagged_ptr.tag(entry_tag::as_homogeneous_ob);
+        tagged_ptr.ptr(ptr);
+    }
 
     Py_ssize_t size() const {
         return static_cast<Py_ssize_t>(entries.size());
-    }
-
-    void set_tag(entry_tag new_tag) {
-        if (tag != entry_tag::as_ob && new_tag == entry_tag::as_ob) {
-            PyObject_GC_Track(this);
-        }
-        else if (tag == entry_tag::as_ob && new_tag != entry_tag::as_ob) {
-            PyObject_GC_UnTrack(this);
-        }
-        tag = new_tag;
     }
 };
 
